54c1e54a63954f458d74c45db80924b14fbaa8de
carloshenriquemkt <falacom.ocarlos@gmail.com>
2023-03-27 12:46:42 -0300

    CSS e html inclusos

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,31 @@ import './App.css'
 
 export default function App() {
   return (
-    <Canvas className='canvas'
-    resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}
-  >
-      <Camera/>
-      <ambientLight intensity={0.5} color={"#ffffff"} />
-      <Suspense fallback={null}>
-      <Michelle />
-      </Suspense>
-      {/* <Ground /> */}
-      <Environment files='bg2.hdr' background/>
-    </Canvas>
+    <>
+        <nav className='menu'>
+          <ul>
+            <li>História</li>
+            <li>Faixas</li>
+            <li>Eventos</li>
+          </ul>
+        </nav>
+    <div className='wrapper'>
+      <div className='content'>
+        <h1>Capoeira</h1>
+        <p>Lorem ipsum</p>
+      </div>
+      <Canvas className='canvas'
+      resize={{ scroll: true, debounce: { scroll: 50, resize: 0 } }}
+      >
+        <Camera/>
+        <ambientLight intensity={0.5} color={"#ffffff"} />
+        <Suspense fallback={null}>
+        <Michelle />
+        </Suspense>
+        {/* <Ground /> */}
+        <Environment files='bg2.hdr' background/>
+      </Canvas>
+    </div>
+    </>
   )
 }
